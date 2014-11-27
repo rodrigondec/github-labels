6068d1a8f50a78b422fe2c3a3999d946c89e5587
Berto Yáñez <bertez@gmail.com>
2014-11-27 10:14:32 +0100

    PEP8

--- manager.py
+++ manager.py
@@ -14,6 +14,7 @@ API_TEMPLATE_URL = 'https://api.github.com/repos/{user}/{repo}/labels'
 REPO_TEMPLATE_URL = 'https://github.com/{user}/{repo}/labels'
 LABELS_FILE = 'labels.json'
 
+
 class LabelManager(object):
     ''' Handles Github Label API '''
     def __init__(self, u, p, r, o):
@@ -31,7 +32,9 @@ class LabelManager(object):
 
     def setLabels(self):
         for label in self.labels:
-            r = requests.post(self.baseUrl, data=json.dumps(label), auth=self.auth)
+            r = requests.post(self.baseUrl, data=json.dumps(label),
+                              auth=self.auth)
+
             if r.status_code != 201:
                 error = 'Error creating label: {0}'.format(r.text)
                 sys.exit(error)
@@ -43,12 +46,14 @@ class LabelManager(object):
                 error = 'Error deleting label: {0}'.format(r.text)
                 sys.exit(error)
 
+
 def main(user, password, repo, organization=None):
     manager = LabelManager(user, password, repo, organization)
     manager.deleteLabels()
     manager.setLabels()
 
-    repo_url = REPO_TEMPLATE_URL.format(user=organization if organization else user, repo=repo)
+    repo_url = REPO_TEMPLATE_URL.format(user=organization if organization
+                                        else user, repo=repo)
 
     print
     print '---'
@@ -58,9 +63,12 @@ if __name__ == '__main__':
     user = raw_input('Please enter your github username: ')
     password = getpass.getpass('Please enter your github password: ')
     repo = raw_input('Please enter the repo name: ')
-    organization = raw_input('If this is a repo from a organization please enter the organization name, if not just press enter: ')
+    organization = raw_input('If this is a repo from a organization please \
+                              enter the organization name, if not just press \
+                              enter: ')
 
-    sure = raw_input('\nPlease enter the word CHANGE to continue. This will delete all your current labels and create a new ones: ')
+    sure = raw_input('\nPlease enter the word CHANGE to continue. This will \
+                      delete all your current labels and create a new ones: ')
 
     if sure.upper() != 'CHANGE':
         sys.exit('Bye')
