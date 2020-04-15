2a0858d4a37f35407e86f512eb43a70fff23621c
rodrigondec <rodrigondec@gmail.com>
2020-04-14 21:05:14 -0300

    remove destructive action

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ if __name__ == '__main__':
     g = Github(TOKEN)
 
     repo = g.get_repo(f"{OWNER}/{REPO}")
-    for label in repo.get_labels():
-        label.delete()
     for label in LABELS:
         repo.create_label(**label)
 
