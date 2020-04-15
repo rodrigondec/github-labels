90273030def7b5f7bbb3ebe2daefa611631fe742
rodrigondec <rodrigondec@gmail.com>
2020-04-15 13:58:37 -0300

    Create or update labels

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from github import Github
+from github import Github, GithubException
 
 from constants import TOKEN, OWNER, REPO, LABELS
 
@@ -8,6 +8,15 @@ if __name__ == '__main__':
     g = Github(TOKEN)
 
     repo = g.get_repo(f"{OWNER}/{REPO}")
-    for label in LABELS:
-        repo.create_label(**label)
 
+    for label in LABELS:
+        try:
+            repo.create_label(**label)
+        except GithubException as e:
+            print(e)
+            repo_label = repo.get_label(label.get('name'))
+            repo_label.edit(
+                name=repo_label.name,
+                color=label.get('color'),
+                description=label.get('description')
+            )
