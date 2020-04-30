3c694a003e7fa2dae7bd16c3ffbd57601faa8a8d
rodrigondec <rodrigondec@gmail.com>
2020-04-30 12:22:05 -0300

    runner without destructive action

--- runner.py
+++ runner.py
@@ -1,9 +1,12 @@
 from main import create_or_update_labels, delete_existing_labels
 
-owner = 'idvogados'
-repos = ['backend', 'frontend']
+owner = 'imobanco'
+repos = [
+    'income-back', 'zoop-wrapper',
+    'autoatendimento', 'pyCNAB240'
+]
 
 for repo in repos:
     print(f'Running for {repo}')
-    delete_existing_labels(owner=owner, repo=repo)
+    # delete_existing_labels(owner=owner, repo=repo)
     create_or_update_labels(owner=owner, repo=repo)
